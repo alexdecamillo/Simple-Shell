5403ddfd49f5e69445e19d8cd6298d09f55f3673
Alex DeCamillo <alexdecamillo@yahoo.com>
2017-12-06 19:34:09 -0500

    Internal and path commands work

--- internalCommands.c
+++ internalCommands.c
@@ -1,5 +1,14 @@
 char* commands[] = {"cd", "quit", "path"};
 
+int checkInternalCommands(char* command) {
+    for (int i = 0; i < (sizeof(commands)/sizeof(char*)); i++) {
+        if (strcmp(commands[i], command) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 void cd(char* directory) {
     chdir(directory);
     // add error handling
@@ -12,22 +21,28 @@ void quit() {
 void path(char* path, int option, char* pathArg) {
     if (!option) {
         // write path to screen
-        write(1, path, len(path));
+        //write(1, path, strlen(path));
+        printf("%s\n", path);
     } else if (option == 1) {
-        if (len(path) != 0)
-            strcat(path, ':');
+        if (strlen(path) != 0)
+            strcat(path, ":");
         strcat(path, pathArg);
     } else if (option == 2) {
         // initialize the command pointers
-        char *tempFullPath = (char *)malloc(5*sizeof(char *));
-        char *pathName = (char *)malloc(5*sizeof(char *));
+        char *tempFullPath = (char *)malloc(100*sizeof(char *));
+        char *pathName = (char *)malloc(10*sizeof(char *));
+        char *buffer = (char *)malloc(100*sizeof(char *));
+        strcpy(buffer, path);
 
-        while ( (pathName = strsep(&buffer, ':')) != NULL ) {
-            if (strcmp(pathName, pathArg) == 0)
-                // maybe output a message
-            else
+        while ( (pathName = strsep(&buffer, ":")) != NULL ) {
+            if (strcmp(pathName, pathArg)) {
+                if (strlen(tempFullPath) != 0)
+                    strcat(tempFullPath, ":");
                 strcat(tempFullPath, pathName);
+            } else {
+                printf("%s%s\n", "removed: ", pathName);
+            }
         }
-        path = tempFullPath;
+        strcpy(path, tempFullPath);
     }
 }
--- main.c
+++ main.c
@@ -5,17 +5,19 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <time.h>
+#include <sys/wait.h>
 #include "internalCommands.c"
 
-int *PATH;
+char *PATH;
 
 void runCommand(char *c[], int len);
 
 int main(int argc, char *argv[]){
 
     int commandLength;
-    fd = malloc(sizeof(int));
-    char prompt[] = "Minix Console$ ";
+    PATH = (char *)malloc(sizeof(char) * 100);
+    //strcat(PATH, "test/with/stuff:blah/blah");
+    char prompt[] = "Simple Shell$ ";
     char *input = malloc(50), *token, *delim=" ", *buffer;
 
     // initialize the command pointers
@@ -23,7 +25,7 @@ int main(int argc, char *argv[]){
     for(int i = 0; i < 2; i++)
         command[i] = (char *)malloc(50*sizeof(char *));
 
-    // the main loop for the minix console
+    // the main loop for the shell
     while (1) {
         // prompt and get input
         printf("%s", prompt);
@@ -47,12 +49,72 @@ int main(int argc, char *argv[]){
 }
 
 // takes the command array and the length of the array and calls the
-//  appropiate module from modules.c
-void runCommand(char *c[], int len){
-    // try to run in internal commands
-    cd(c);
-    // then try to run in path commands
-    // else print error
+//  appropiate command
+void runCommand(char *argv[], int argc){
+    // check if command is an interal command first
+    // otherwise, check if it is in the path
 
+    if (checkInternalCommands(argv[0])) {
+        if (!strcmp(argv[0], "quit")) {
+            quit();
+        } else if(!strcmp(argv[0], "cd")) {
+            if (argc >= 2) {
+                cd(argv[1]);
+            } else {
+                printf("You must specify a directory location\n");
+            }
+        } else if (!strcmp(argv[0], "path")) {
+            if (argc == 1) {
+                path(PATH, 0, NULL);
+            } else if (argc == 3) {
+                if (!strcmp(argv[1], "+")) {
+                    path(PATH, 1, argv[2]);
+                } else if(!strcmp(argv[1], "-")) {
+                    path(PATH, 2, argv[2]);
+                }
+            } else {
+                printf("%s%d%s\n", "path: ERROR: no command for 'path' takes ", argc, " arguments");
+            }
+        }
+    } else {
+        // check if command is in a path directory
+
+        // fork
+        pid_t pid = fork();
+
+        // check child or parent
+        if(pid < 0) {
+            // error
+            perror(argv[0]);
+        } else if(pid == 0) {
+            // child
+            char *command = malloc(sizeof(char* ) * 20);
+
+            // initialize the command pointers
+            char *pathName = (char *)malloc(10*sizeof(char *));
+            char *buffer = (char *)malloc(100*sizeof(char *));
+            char *commandName = (char *)malloc(100*sizeof(char *));
+            strcpy(buffer, PATH);
+            strcpy(commandName, argv[0]);
+
+            while ( (pathName = strsep(&buffer, ":")) != NULL ) {
+                // construct argument
+                strcpy(command, pathName);
+                strcat(command, "/");
+                strcat(command, commandName);
+                argv[0] = command;
+                argv[argc] = NULL;
+
+                execve(argv[0], argv, NULL);
+            }
+
+            printf("Error: could not find command '%s' in the path\n", commandName);
+            exit(0);
+        } else {
+            // parent
+            int status;
+            waitpid(pid, &status, 0);
+        }
+    }
 
 }
